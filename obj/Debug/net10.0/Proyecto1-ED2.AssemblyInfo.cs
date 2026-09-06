71bad5774c0b9a0c2322610022b99c4caab7d694
Maykmora <maykmora12@gmail.com>
2026-09-06 17:41:08 -0600

    Estructura de nodoBplus y funciones de arbolBplus

--- obj/Debug/net10.0/Proyecto1-ED2.AssemblyInfo.cs
+++ obj/Debug/net10.0/Proyecto1-ED2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1-ED2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc94ef96b86bd5b94a36d8d6062ca1d801a5dd3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3defeca4d87486042eb49bc1ae35eefddaeeb983")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1-ED2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1-ED2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
