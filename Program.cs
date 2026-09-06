3defeca4d87486042eb49bc1ae35eefddaeeb983
Maykmora <maykmora12@gmail.com>
2026-09-06 01:20:44 -0600

    Estructura del libro

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+public class Libro
+{
+    public string Codigo { get; set; }
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string Categoria { get; set; }
+    public int CopiasDisponibles { get; set; }
+    public int VecesPrestado { get; set; }
+
+    public Libro(string codigo, string titulo, string autor, string categoria, int copiasDisponibles)
+    {
+        Codigo = codigo;
+        Titulo = titulo;
+        Autor = autor;
+        Categoria = categoria;
+        CopiasDisponibles = copiasDisponibles;
+        VecesPrestado = 0;
+    }
+}
